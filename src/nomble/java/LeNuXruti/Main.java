a5cb5d39c703df5456817b32ff7875715ec42688
BuildTools <unconfigured@null.spigotmc.org>
2017-01-06 22:17:44 -0500

    Initial commit.

---
+++ src/nomble/java/LeNuXruti/Main.java
@@ -0,0 +1,15 @@
+package nomble.java.LeNuXruti;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnabled(){
+		
+	}
+
+	@Override
+	public void onDisabled(){
+		
+	}
+}
