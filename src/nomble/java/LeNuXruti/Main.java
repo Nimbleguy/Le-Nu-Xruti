b8564ad2f633fd35dcc4a1fef40d3fe42d3a95b5
Nimbleguy <nomblest@gmail.com>
2017-01-07 21:27:06 -0500

    Now builds.

--- src/nomble/java/LeNuXruti/Main.java
+++ src/nomble/java/LeNuXruti/Main.java
@@ -1,7 +1,6 @@
 package nomble.java.LeNuXruti;
 
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecuter;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,12 +11,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
 import java.util.UUID;
 
-public class Main extends JavaPlugin implements CommandExecuter, Listener{
+public class Main extends JavaPlugin implements Listener{
 	public HashMap<UUID, String> suffix = new HashMap<UUID, String>();
 	public HashMap<UUID, String> nick = new HashMap<UUID, String>();
 	public HashMap<UUID, String> prefix = new HashMap<UUID, String>();
 	@Override
-	public void onEnabled(){
+	public void onEnable(){
 		this.getCommand("prefix").setExecutor(this);
 		this.getCommand("nick").setExecutor(this);
 		this.getCommand("suffix").setExecutor(this);
@@ -27,11 +26,11 @@ public class Main extends JavaPlugin implements CommandExecuter, Listener{
 	}
 
 	@Override
-	public void onDisabled(){}
+	public void onDisable(){}
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event){
-		event.player.setDisplayName("[" + getNoNull(prefix.get(p.getUniqueId(), "")) + "] (" + getNoNull(nick.get(p.getUniqueId(), p.getPlayerListName()) + ") " + getNoNull(suffix.get(p.getUniqueId(), "")));
+		setNick(event.getPlayer());
 	}
 
 	@Override
@@ -39,11 +38,11 @@ public class Main extends JavaPlugin implements CommandExecuter, Listener{
 		if(label.startsWith("u") && args.length > 1){
 			StringBuilder s = new StringBuilder();
 			s.append(args[1]);
-			for(int i = 2; i < args.length, i++){
+			for(int i = 2; i < args.length; i++){
 				s.append(" ");
 				s.append(args[i]);
 			}
-			String str = s.toString()
+			String str = s.toString();
 			Player p = this.getServer().getPlayerExact(args[0]);
 			if(label.equalsIgnoreCase("uprefix")){
 				prefix.put(p.getUniqueId(), str);
@@ -57,16 +56,16 @@ public class Main extends JavaPlugin implements CommandExecuter, Listener{
 			else{
 				return false;
 			}
-			p.setDisplayName("[" + getNoNull(prefix.get(p.getUniqueId(), "")) + "] (" + getNoNull(nick.get(p.getUniqueId(), p.getPlayerListName()) + ") " + getNoNull(suffix.get(p.getUniqueId(), "")));
+			setNick(p);
 		}
 		else if(sender instanceof Player && args.length > 0){
 			StringBuilder s = new StringBuilder();
 			s.append(args[0]);
-			for(int i = 1; i < args.length, i++){
+			for(int i = 1; i < args.length; i++){
 				s.append(" ");
 				s.append(args[i]);
 			}
-			String str = s.toString()
+			String str = s.toString();
 			Player p = (Player)sender;
 			if(label.equalsIgnoreCase("prefix")){
 				prefix.put(p.getUniqueId(), str);
@@ -75,12 +74,12 @@ public class Main extends JavaPlugin implements CommandExecuter, Listener{
 				nick.put(p.getUniqueId(), str);
 			}
 			else if(label.equalsIgnoreCase("suffix")){
-				suffix.put(p.getUniqueId, str());
+				suffix.put(p.getUniqueId(), str);
 			}
 			else{
 				return false;
 			}
-			p.setDisplayName("[" + getNoNull(prefix.get(p.getUniqueId(), "")) + "] (" + getNoNull(nick.get(p.getUniqueId(), p.getPlayerListName()) + ") " + getNoNull(suffix.get(p.getUniqueId(), "")));
+			setNick(p);
 		}
 		else{
 			return false;
@@ -90,5 +89,15 @@ public class Main extends JavaPlugin implements CommandExecuter, Listener{
 
 	private String getNoNull(String v, String def){
 		return v == null ? def : v;
+	}
+
+	private void setNick(Player p){
+		String pref = "[" + getNoNull(prefix.get(p.getUniqueId()), "") + "]";
+		String cage = "(" + getNoNull(nick.get(p.getUniqueId()), "") + ")";
+		if(cage == "()"){
+			cage = p.getPlayerListName();
+		}
+		String suff = getNoNull(suffix.get(p.getUniqueId()), "");
+		p.setDisplayName(pref + cage + suff);
 	}
 }
